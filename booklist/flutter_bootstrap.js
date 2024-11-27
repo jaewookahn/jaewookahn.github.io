02bb3693a81d230906ea8c1de1153c69cc651453
Jae-wook Ahn <jaewook.ahn@gmail.com>
2024-11-27 04:35:34 +0000

    Deploy Flutter web app

--- booklist/flutter_bootstrap.js
+++ booklist/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3273751789"
+    serviceWorkerVersion: "3122964775"
   }
 });
